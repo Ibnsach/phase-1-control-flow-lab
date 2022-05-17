6e88550bd39f1c2e2953dda8e94af558a86fe34d
Adam Abass <adam.imoro@student.moringaschool.com>
2022-05-17 06:15:01 +0000

    commit tree c0a8abfa4d8a33572705a77c1df7f48f48f7ce8d

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(feet){
   // Write your code here;
     if(feet <= 400){
       return "This one is on me!";
-    } else if(feet > 400 && feet < 2000){
+    } else if(feet > 400, feet < 2000){
       return "That will be twenty bucks.";
     } else if(feet < 2500){
       return "I will gladly take your thirty bucks."
